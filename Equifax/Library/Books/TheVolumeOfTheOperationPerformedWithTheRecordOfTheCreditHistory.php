76e0f05ed4224baf30e6e80303b6139fa8ac3d36
Алексей Зорин <zorinalexey59292@gmail.com>
2022-11-07 19:14:28 +0400

    start library

---
+++ Equifax/Library/Books/TheVolumeOfTheOperationPerformedWithTheRecordOfTheCreditHistory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Equifax\ReferenceBooks\Books;
+
+use \Equifax\ReferenceBooks\Books;
+
+/**
+ * Класс TheVolumeOfTheOperationPerformedWithTheRecordOfTheCreditHistory
+ * @version 0.0.1
+ * @package Equifax\ReferenceBooks\Books\TheVolumeOfTheOperationPerformedWithTheRecordOfTheCreditHistory
+ * @generated Зорин Алексей, please DO NOT EDIT!
+ * @author Зорин Алексей <zorinalexey59292@gmail.com>
+ * @copyright 2022 разработчик Зорин Алексей Евгеньевич.
+ */
+class TheVolumeOfTheOperationPerformedWithTheRecordOfTheCreditHistory
+{
+
+    use Books;
+
+    /**
+     * Значение по умолчанию
+     * @var string
+     */
+    private static int $default = 1;
+
+    /**
+     * Коллекция возможных значений
+     * @var array
+     */
+    private static array $data = [
+        'изменение отдельных показателей кредитной истории' => 1,
+        'исключение/аннулирование отдельных показателей кредитной истории' => 2,
+        'исключение/аннулирование записи кредитной истории' => 3,
+        'исключение/аннулирование всех показателей кредитной истории' => 4,
+        'представляется непринятая бюро кредитная информация' => 5
+    ];
+
+}
